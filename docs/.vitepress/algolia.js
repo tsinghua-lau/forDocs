cd7341a6051878960ea0d924807fb772f78cc131
“tsinghua-lau <408285649@qq.com>
2023-02-08 09:40:08 +0800

    feat:添加全局搜索

---
+++ docs/.vitepress/algolia.js
@@ -0,0 +1,7 @@
+export default {
+  appId: "U96O2E9ZIZ",
+  apiKey: "94e81089b3e85688c237e5137334b807",
+  indexName: "zs-design-ui",
+  placeholder: "请输入关键词",
+  buttonText: "搜索",
+};
--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,9 +1,10 @@
+import algolia from "./algolia";
+
 // base是我们的发布路径，默认为/，可以在环境变量中自行配置
 const base = process.env.BASE || "/";
 // nav导航我们抽离在configs下面nav.js文件，方便管理
 const nav = require("./configs/nav");
 const sidebar = require("./configs/sidebar");
-
 module.exports = {
   title: "@瞎折腾",
   description: "Life is short, Keep it simple.",
@@ -14,6 +15,7 @@ module.exports = {
   themeConfig: {
     nav,
     sidebar,
+    algolia,
     editLinks: true,
     lastUpdated: "上次更新",
 
