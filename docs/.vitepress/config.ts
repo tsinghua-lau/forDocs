15d2acac7b6252b76d5c0bc36824c9f891d64012
408285649@qq.com <408285649@qq.com>
2023-04-22 10:41:31 +0800

    update:configuratio

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,26 +6,26 @@ import algolia from "./algolia";
 import socialLinks from "./link";
 
 export default defineConfig({
+  head,
   title: "ForDocs",
   description: "VitePress",
-  head,
-  lastUpdated: true,
-  // outDir: "../dist",
   themeConfig: {
-    logo: "/images/logo.svg",
     nav,
     sidebar,
-    outlineTitle: "本页目录",
     algolia,
-    // editLinks: true,
-    // outline: "deep",
+    socialLinks,
+    outlineTitle: "本页目录",
+    logo: "/images/logo.svg",
     editLink: {
       pattern:
         "https://github.com/tsinghua-lau/tsinghua-lau.github.io/tree/dev/docs/:path",
 
       text: "在 GitHub 上编辑此页",
     },
-    socialLinks,
+    // lastUpdated: true,
+    // outDir: "../dist",
+    // editLinks: true,
+    // outline: "deep",
     // footer: {
     //   message: "Released under the MIT License.",
     //   copyright: "XXX",
