914fe191728184780dc934f1aa33ebf1fcd828c2
tsinghua-lau <tsinghua-lau@users.noreply.github.com>
2023-05-13 13:19:18 +0000

    deploy: 8eb08963520fbf3c06898eafda30b3cea4192030

--- assets/index.md.ed172e33.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o,c as a,b as n,d as r}from"./app.e7cff7dd.js";const f=JSON.parse('{"title":"ForDocs","description":"","frontmatter":{"layout":"home","title":"ForDocs"},"headers":[],"relativePath":"index.md"}'),s={name:"index.md"};function c(_,d,i,p,m,l){const e=r;return o(),a("div",null,[n(e)])}var x=t(s,[["render",c]]);export{f as __pageData,x as default};
